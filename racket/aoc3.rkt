616cc7a312a91bc5f9c24a2c7cea4c8422f742d3
Donnie Mattingly <donniewmattingly@gmail.com>
2018-12-03 16:26:26 -0500

    part one solution (I think); adding haskell setup info

--- racket/aoc3.rkt
+++ racket/aoc3.rkt
@@ -20,10 +20,16 @@
           [id (parse-id (list-ref srow 0))] 
           [offsets (parse-offsets (list-ref srow 2))] 
           [size (parse-size (list-ref srow 3))])
-      (claim id (car offsets) (cdr offsets) (car size) (cdr size)))))
+      (claim id (car offsets) (car (cdr offsets)) (car size) (car (cdr size))))))
+
+(define (test-input)
+  (file->lines "../inputs/input-3.0.txt"))
 
 (define (input)
   (file->lines "../inputs/input-3.1.txt"))
+
+(define (parsed-test-input)
+  (map parse-row (test-input)))
 
 (define (parsed-input)
   (map parse-row (input)))
@@ -34,6 +40,43 @@
 (define (max-loffset claims)
   (apply max (map claim-lOff claims)))
 
+(define (apply-claim arr claim)
+  (let (
+        [left-offset (claim-lOff claim)] 
+        [top-offset (claim-tOff claim)]
+        [width (claim-w claim)]
+        [height (claim-h claim)])
+      (let ([indices (get-claim-area-indices left-offset top-offset width height)])
+        (map (lambda (idx) (apply-claim-portion arr idx)) indices))))
+
+(define (apply-claim-portion arr indices)
+  (let ([cur-val (array-ref arr indices)])
+    (array-set! arr indices (+ 1 cur-val))))
+
+(define (get-claim-area-indices x y w h)
+  (let ([indices (apply append (for/list ([i (in-range x (+ x w))])
+                          (for/list ([j (in-range y (+ y h))])
+                            (vector i j))))])
+    indices))
+
+(define (pairs x y)
+  (map (λ (i j) (vector i j)) x y))
+
 (define (part-one input)
-  0 )
+  (let ([max-lOff (max-claim-field-by input claim-lOff)]
+        [max-tOff (max-claim-field-by input claim-tOff)]
+        [max-w (max-claim-field-by input claim-w)]
+        [max-h (max-claim-field-by input claim-h)])
+    (let (
+          [arr 
+           (array->mutable-array (build-array 
+                                  (quasiquote #((unquote (+ max-lOff max-w)) (unquote (+ max-tOff max-h)))) 
+                                  (lambda (x) 0)
+                                  ))
+           ]
+          )
+      (map (lambda (claim) (apply-claim arr claim)) input)
+
+      (sum (map (lambda (x) (if (> x 1) 1 0)) (array->list arr)))
+)))
 
