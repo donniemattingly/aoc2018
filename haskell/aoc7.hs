f7d060b57f1b90c43715f29bcd75f5db2ce85de9
Donnie Mattingly <donniewmattingly@gmail.com>
2018-12-07 00:28:11 -0500

    WIP day 7

--- haskell/aoc7.hs
+++ haskell/aoc7.hs
@@ -1,14 +1,28 @@
 import System.IO  
 import qualified Data.Text as T
+import qualified Data.Map as Map
 
-testFileName = "../inputs/input-6.0.txt"
-  
+testFileName = "../inputs/input-7.0.txt"
+fileName = "../inputs/input-7.1.txt"
+
 main = do  
     handle <- openFile testFileName ReadMode  
     contents <- hGetContents handle  
-    print $ readlines contents  
+    let vals = map condenseDependency (readlines contents)
+    print $ aggregateDependencies vals
     hClose handle  
 
 readlines :: String -> [String]
 readlines contents = 
     map T.unpack $ T.splitOn (T.pack "\n") (T.pack contents)
+
+
+condenseDependency :: String -> [String]
+condenseDependency dependencyStr =
+    filter (\x -> (length x) == 1) (map T.unpack (T.splitOn (T.pack " ") (T.pack dependencyStr)))
+
+
+aggregateDependencies :: [[String]] -> [(String, [String])]
+aggregateDependencies dependencies = 
+    let reversed = map reverse dependencies in
+        map (\x -> (head x, tail x)) reversed
