1e1cf418bd944e67b11b3b34f6dc979091a73a7f
Donnie Mattingly <donniewmattingly@gmail.com>
2018-12-10 09:40:14 -0500

    Correct input parsing

--- clojure/aoc10/src/aoc10/core.clj
+++ clojure/aoc10/src/aoc10/core.clj
@@ -8,7 +8,13 @@
 (defn parse-digits
   [input]
   (apply str 
-         (filter #(#{\0,\1,\2,\3,\4,\5,\6,\7,\8,\9,\-,\space} %) input)))
+         (filter #(#{\0,\1,\2,\3,\4,\5,\6,\7,\8,\9,\-,\space,\,} %) input)))
+
+(defn coordpair->particle
+  [coordpair]
+  (let [pos (first coordpair)
+        dpos (second coordpair)]
+    {:x (first pos) :y (second pos) :dx (first dpos) :dy (second dpos)}))
 
 (defn parse-line
   [line]
@@ -18,9 +24,9 @@
         coords  (map 
                  (comp #(str/split % #",") #(parse-digits %)) 
                  (take 2 (re-seq coordRegex line)))
+        particles (coordpair->particle coords)
         ]
-    (println strCoords)
-    coords)
+    particles)
   )
 
 (defn parse-input
