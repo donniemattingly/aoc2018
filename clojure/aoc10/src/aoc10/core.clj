83097b8372b228bad115f69a42705c5417bf258d
Donnie Mattingly <donniewmattingly@gmail.com>
2018-12-10 08:24:46 -0500

    Input parsing part 1

--- clojure/aoc10/src/aoc10/core.clj
+++ clojure/aoc10/src/aoc10/core.clj
@@ -5,7 +5,27 @@
   []
   (println "Test"))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn parse-digits
+  [input]
+  (apply str 
+         (filter #(#{\0,\1,\2,\3,\4,\5,\6,\7,\8,\9,\-,\space} %) input)))
+
+(defn parse-line
+  [line]
+  (let [
+        coordRegex #"\<\s*-*\d*,\s*-*\d*\>"
+        strCoords (re-find coordRegex line)
+        coords  (map 
+                 (comp #(str/split % #",") #(parse-digits %)) 
+                 (take 2 (re-seq coordRegex line)))
+        ]
+    (println strCoords)
+    coords)
+  )
+
+(defn parse-input
+  [filename]
+  (let [contents (str/split (slurp filename) #"\n")
+        coords (map #(parse-line %) contents)]
+    (map #(println %) coords)))
+
