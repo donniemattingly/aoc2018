ccfe722e290e15a5725ae21dbf74860eab82edf6
Donnie Mattingly <donniewmattingly@gmail.com>
2018-12-14 21:46:14 -0500

    Naive solution to part 2. Slow.

--- elm/sample-app/src/Main.elm
+++ elm/sample-app/src/Main.elm
@@ -68,6 +68,47 @@ initGetRecipes count =
     in
         getRecipes 2 count elf1 elf2 initialRecipes
 
+
+testPattern : String -> Array Int -> Bool
+testPattern pattern array =
+    let len = String.length pattern
+        arrayLen = Array.length array
+        testStr = printableArray(Array.slice (-1 * len) arrayLen array)
+    in
+        pattern == testStr
+
+
+getRecipes2 : Int -> String -> Int -> Int -> Array Int -> Array Int
+getRecipes2 curCount endPattern elf1 elf2 recipes =
+    -- let _ = Debug.log "recipes: " (printableArray recipes)  in
+    -- let _ = Debug.log "elf1: " elf1 in
+    -- let _ = Debug.log "elf2: " elf2 in 
+
+    let _ = 
+            if (modBy 1000 curCount) == 0 then
+                Debug.log "count" curCount
+            else
+                curCount
+    in
+
+    if testPattern endPattern recipes then
+        recipes
+    else
+        let (new1, new2, newRecipes) = nextRecipe elf1 elf2 recipes 
+            recipeCount = Array.length newRecipes
+        in
+            getRecipes2 (Array.length newRecipes) endPattern new1 new2 newRecipes
+
+initGetRecipes2 : String -> Array Int
+initGetRecipes2 pattern =
+    let 
+        initialRecipes = Array.fromList [3, 7] 
+        elf1 = 0
+        elf2 = 1
+    in
+        getRecipes2 2 pattern elf1 elf2 initialRecipes
+
+
 partOne : Int -> String
 partOne countTo = 
     let 
@@ -78,11 +119,16 @@ partOne countTo =
     in
         printableArray lastSlice
     
-        
-    
+partTwo : String -> Int
+partTwo pattern = 
+    let recipes = initGetRecipes2 pattern
+        leftSize = (Array.length recipes) - (String.length pattern)
+    in
+        leftSize
+   
 main = 
     let 
-        result = partOne 825401
+        result = partTwo "825401"
         _ = Debug.log "result" result
     in
     Html.text "Hello World"
