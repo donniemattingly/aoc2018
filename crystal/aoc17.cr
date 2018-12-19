51c673e9795013a69eaa565b165adf84c57de8a3
Donnie Mattingly <donniewmattingly@gmail.com>
2018-12-19 01:03:42 -0500

    day 17

--- crystal/aoc17.cr
+++ crystal/aoc17.cr
@@ -1,5 +1,5 @@
 def input
-  `cat ../inputs/input-17.0.txt`
+  `cat ../inputs/input-17.1.txt`
     .split("\n")
     .reject(&.empty?)
     .compact
@@ -45,16 +45,79 @@ end
 
 points = input()
 lower, upper = bounds(points)
-height = (upper[1] - lower[1]) + 1
-width = (upper[0] - lower[0]) + 1
-adjusted_points = points.map { |e| {e[0] - lower[0], e[1] - lower[1]} }
+height = (upper[1] - lower[1]) + 2
+width = (upper[0] - lower[0]) + 2
+adjusted_points = points.map { |e| {e[0] - (lower[0] - 1), e[1] - lower[1]} }
 
-puts "h: #{height} w: #{width}"
+# puts "h: #{height} w: #{width}"
 matrix = Array.new(height) { |i| Array.new(width) { |j| '.' } }
 
 adjusted_points.each do |p|
-  puts p
   matrix[p[1]][p[0]] = '#'
 end
 
-disp_board matrix
+# rules for water spreading to adjacent cells
+# assumption is that point has water
+def fill(board, point, height)
+  x, y = point
+  if (y >= height - 2)
+    return
+  end
+
+  # below is sand
+  if (board[y + 1][x] == '.')
+    board[y + 1][x] = '|'
+    fill(board, {x, y + 1}, height)
+  end
+
+  # below is settled water or clay
+  if (board[y + 1][x] == '#' || board[y + 1][x] == '~')
+    # sand on left
+    if (board[y][x - 1] == '.')
+      board[y][x - 1] = '|'
+      fill(board, {x - 1, y}, height)
+    end
+
+    # sand on right
+    if (board[y][x + 1] == '.')
+      board[y][x + 1] = '|'
+      fill(board, {x + 1, y}, height)
+    end
+
+    # check for reservoirs
+    left = '0'
+    lOffset = 1
+    right = '0'
+    rOffset = 1
+
+    while left == '0'
+      c = board[y][x - lOffset]
+      if c == '#' || c == '.'
+        left = c
+        break
+      end
+      lOffset += 1
+    end
+
+    while (right == '0')
+      c = board[y][x + rOffset]
+      if c == '#' || c == '.'
+        right = c
+        break
+      end
+      rOffset += 1
+    end
+
+    # puts "#{right}, #{left}"
+
+    if (left == '#' && right == '#')
+      ((x - (lOffset - 1))..(x + (rOffset - 1))).each { |e| board[y][e] = '~' }
+    end
+  end
+end
+
+fill(matrix, {500 - lower[0], 0}, height)
+
+puts matrix.flatten.reject { |x| x == '.' || x == '#' }.size + 1
+
+puts matrix.flatten.select { |x| x == '~' }.size
