386fef8d76e2928a160a1ee5d6ea08829803d3bf
Donnie Mattingly <donniewmattingly@gmail.com>
2018-12-22 09:30:06 -0500

    adding more failed code

--- go/aoc20.go
+++ go/aoc20.go
@@ -1,7 +1,6 @@
 package main
 
 import "fmt"
-import "regexp"
 import "io/ioutil"
 import "strings"
 
@@ -12,76 +11,62 @@ func main() {
 		fmt.Print(err)
 	}
 	var input = strings.Trim(string(b), " \n")
-	fmt.Println(input)
-	var size = foo(input)
+
+	var size = partOne(input)
 	fmt.Println(size)
 }
 
-var option = regexp.MustCompile(`\(.*\)`)
 
-/*
-
+/* stack implementation from:
+	https://stackoverflow.com/questions/28541609/looking-for-reasonable-stack-implementation-in-golang
  */
 
-func tokenizeString(x string) []string {
-	var paren_depth = 0
-	var result []string
-	var group_start = 0
-	for pos, char := range x {
-		if char == '(' {
-			paren_depth++
-			group_start = pos
-		} else if char == ')' {
-			paren_depth--
-			if(paren_depth == 0){
-				result = append(result, string(x[group_start:pos]))
-			}
-		}
-	}
+type stack []int
 
-	return result
+func (s stack) Push(v int) stack {
+	return append(s, v)
 }
-func longestOptionSize(x string) int{
-	//var result = option.FindString(x)
 
-	var paren_depth = 0
-	var result []string
-	var group_start = 0
-	for pos, char := range x {
-		if char == '(' {
-			paren_depth++
-			group_start = pos
-		} else if char == ')' {
-			paren_depth--
-			if(paren_depth == 0){
-				result = append(result, string(x[group_start:pos]))
-			}
-		}
-	}
+func (s stack) Pop() (stack, int) {
+	// FIXME: What do we do if the stack is empty, though?
 
-	if(result == ""){
-		return len(x)
-	}
-
-	var base = len(x) - len(result)
-
-
-	var stripped = result[1:len(result) - 1]
-	//fmt.Println(stripped)
-	var max = 0
-	var split = splitTopLevel(stripped)
-
-	for _, elem := range split {
-		var size = longestOptionSize(elem)
-		if size > max{
-			max = size
-		}
-	}
-
-	return base + max
+	l := len(s)
+	return  s[:l-1], s[l-1]
 }
+
+func (s stack) TakeLast() (stack, int) {
+	return  s[1:], s[0]
+}
+
+func partOne(s string) int{
+	var moves stack
+	var distances [] int
+	var dist = 0
+	for _, char := range s {
+		if char == '(' {
+			moves = moves.Push(dist)
+		} else if char == ')' {
+			moves, dist = moves.Pop()
+		} else if char == '|' {
+			dist = moves[0]
+		} else {
+			dist = dist + 1
+			distances = append(distances, dist)
+		}
+	}
+
+	var max_dist = 0
+	for _, dist := range distances {
+		if dist > max_dist {
+			max_dist = dist
+		}
+	}
+
+	return max_dist
+}
+
 func splitTopLevel(s string) []string {
-	var result []string 
+	var result []string
 	var paren_depth = 0
 	var last_split = 0
 	for pos, char := range s {
@@ -102,41 +87,56 @@ func splitTopLevel(s string) []string {
 }
 
 func foo(s string) int {
+	//fmt.Println(s)
 	var base []string
 	var options []string
-	var paren_depth = 0
-	var last_split = 0
+	var parenDepth = 0
+	var lastSplit = 0
 	for pos, char := range s {
 		if char == '(' {
-			if paren_depth == 0 {
-				base = append(base, string(s[last_split:pos-1]))
-				last_split = pos + 1
+			if parenDepth == 0 && lastSplit != pos {
+				//fmt.Printf("%s ls: %d size:%d pos:%d \n",s, lastSplit, len(s), pos)
+				base = append(base, string(s[lastSplit:pos]))
+				lastSplit = pos + 1
 			}
-			paren_depth++
+			parenDepth++
 		} else if char == ')' {
-			paren_depth--
-			if paren_depth == 0 {
-				options = append(options, string(s[last_split:pos]))
-				last_split = pos + 1
+			parenDepth--
+			if parenDepth == 0 {
+				var moreOpts = splitTopLevel(string(s[lastSplit:pos]))
+				options = append(options, moreOpts...)
+				lastSplit = pos + 1
 			}
-		} else if char == '|' && paren_depth == 0 {
-			options = append(options, string(s[last_split:pos]))
-			last_split = pos + 1
 		}
 	}
 
-	base = append(base, string(s[last_split]))
+	if lastSplit < len(s) {
+		var newS = string(s[lastSplit:len(s)])
+		base = append(base, newS)
+	}
+
+	//fmt.Println("opt:")
+	//fmt.Println(options)
 	var size = 0
 	for _, val := range options {
-		var cur_size = foo(val)
-		if cur_size > size {
-			size = cur_size
+		var curSize = foo(val)
+		//fmt.Printf("val: %s\n", val)
+		if curSize > size {
+			size = curSize
 		}
 	}
 
-	var total = len(base) + size
+	//fmt.Println("base:")
+	//fmt.Println(base)
+	var total = size
+	for _, val := range base {
+		//fmt.Printf("base: %s\n", val)
+		total = total + len(val)
+	}
 	// fmt.Println("----------")
 	// fmt.Println(s)
 	// fmt.Println(result)
+
+	fmt.Printf("size: %d for %s\n", total, s)
 	return total
 }
