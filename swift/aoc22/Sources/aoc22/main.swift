f890e031abd30d70975834a37a8246b1ae1be2d5
Donnie Mattingly <donniewmattingly@gmail.com>
2018-12-28 09:03:52 -0500

    day 22 progress

--- swift/aoc22/Sources/aoc22/main.swift
+++ swift/aoc22/Sources/aoc22/main.swift
@@ -5,11 +5,11 @@ struct Terrain {
     var depth: Int
 }
 
-func generateTerrain(x: Int, y: Int, depth: Int) -> Terrain{
+func generateTerrain(x: Int, y: Int, depth: Int) -> Terrain {
     var geo = Array(repeating: Array(repeating: 0, count: x + 1), count: y + 1)
 
     for i in 0...x {
-        for j in 0...y{
+        for j in 0...y {
             var geoIndex = 0
             if i == x && j == y {
                 geoIndex = 0
@@ -30,14 +30,14 @@ func generateTerrain(x: Int, y: Int, depth: Int) -> Terrain{
     return Terrain(geo: geo, depth: depth)
 }
 
-func printTerrain(terrain: Terrain){
+func printTerrain(terrain: Terrain) {
     let repr = terrain.geo.map { rows -> String in
         rows.map { erosionLevel -> String in
             let cell = erosionLevel % 3
             var sym = ""
-            if(cell == 0){
+            if (cell == 0) {
                 sym = "."
-            } else if(cell == 1){
+            } else if (cell == 1) {
                 sym = "="
             } else {
                 sym = "|"
@@ -50,10 +50,10 @@ func printTerrain(terrain: Terrain){
     print(repr)
 }
 
-func getRiskLevel(terrain: Terrain, x: Int, y: Int) -> Int{
+func getRiskLevel(terrain: Terrain, x: Int, y: Int) -> Int {
     var riskLevel = 0
     for i in 0...x {
-        for j in 0...y{
+        for j in 0...y {
             riskLevel += (terrain.geo[j][i] % 3)
         }
     }
@@ -61,16 +61,13 @@ func getRiskLevel(terrain: Terrain, x: Int, y: Int) -> Int{
     return riskLevel
 }
 
-struct Input{
+struct Input {
     var x: Int
     var y: Int
     var depth: Int
 }
 
-let testInput = Input(x: 10, y: 10, depth: 510)
-let realInput = Input(x: 7, y: 701, depth: 11394)
-
-func partOne(input: Input){
+func partOne(input: Input) {
     let terrain = generateTerrain(x: input.x, y: input.y, depth: input.depth)
     let riskLevel = getRiskLevel(terrain: terrain, x: input.x, y: input.y)
     print("Part One: \(riskLevel)")
@@ -90,11 +87,11 @@ struct Vertex: Equatable {
     var type: TerrainType
     var tool: Tool
 
-    static func == (lhs: Vertex, rhs: Vertex) -> Bool {
+    static func ==(lhs: Vertex, rhs: Vertex) -> Bool {
         return lhs.x == rhs.x &&
-                lhs.y == rhs.y &&
-                lhs.type == rhs.type &&
-                lhs.tool == rhs.tool
+        lhs.y == rhs.y &&
+        lhs.type == rhs.type &&
+        lhs.tool == rhs.tool
     }
 }
 
@@ -102,9 +99,9 @@ struct Point: Equatable, Hashable {
     var x: Int
     var y: Int
 
-    static func == (lhs: Point, rhs: Point) -> Bool {
+    static func ==(lhs: Point, rhs: Point) -> Bool {
         return lhs.x == rhs.x &&
-                lhs.y == rhs.y
+        lhs.y == rhs.y
     }
 
     func hash(into hasher: inout Hasher) {
@@ -123,10 +120,11 @@ func terrainToVertices(terrain: Terrain) -> ([Vertex], Dictionary<Point, [Vertex
         for i in 0..<arr[0].count {
             let terrainType = arr[j][i] % 3
             var v: [Vertex] = []
-            if(terrainType == 0){
+
+            if (terrainType == 0) {
                 // Rocky (Climbing / Torch)
                 v = [Vertex(x: i, y: j, type: .Rocky, tool: .Climbing), Vertex(x: i, y: j, type: .Rocky, tool: .Torch)]
-            } else if(terrainType == 1){
+            } else if (terrainType == 1) {
                 // Wet (Climbing / Neither)
                 v = [Vertex(x: i, y: j, type: .Wet, tool: .Climbing), Vertex(x: i, y: j, type: .Wet, tool: .Neither)]
             } else {
@@ -142,34 +140,75 @@ func terrainToVertices(terrain: Terrain) -> ([Vertex], Dictionary<Point, [Vertex
     return (vertices, gridDict)
 }
 
-func generateGraph(terrain: Terrain) -> WeightedGraph<Vertex, Int> {
+func timeToMoveTo(v1: Vertex, v2: Vertex) -> Int {
+    if v1.type == v2.type {
+        return 1
+    } else if v1.tool == v1.tool {
+        return 7
+    } else {
+        return -1
+    }
+}
+
+func generateGraph(terrain: Terrain) -> (WeightedGraph<Vertex, Int>, Dictionary<Point, [Vertex]>) {
     let (vertices, gridDict) = terrainToVertices(terrain: terrain)
 
     // add all vertices
     let graph = WeightedGraph<Vertex, Int>(vertices: vertices)
 
     for j: Int in 0..<terrain.geo.count {
-        for i: Int in 0..<terrain.geo[0].count{
+        for i: Int in 0..<terrain.geo[0].count {
             let v = gridDict[Point(x: i, y: j)]!
             let neighbors: [Vertex] = [
-                gridDict[i - 1, j],
-                gridDict[i + 1, j],
-                gridDict[i, j - 1],
-                gridDict[i, j + 1]
-            ].flatMap { (vs: [Vertex]?) -> [Vertex]? in
+                gridDict[Point(x: i - 1, y: j)],
+                gridDict[Point(x: i + 1, y: j)],
+                gridDict[Point(x: i, y: j - 1)],
+                gridDict[Point(x: i, y: j + 1)]
+            ].flatMap { (vs: [Vertex]?) -> [Vertex] in
                 vs ?? []
             }
+
+            for vertex in v {
+                let validNeighbors = neighbors.map { (n: Vertex) -> (Vertex, Int) in
+                    (n, timeToMoveTo(v1: n, v2: vertex))
+                }.filter { (n: Vertex, time: Int) -> Bool in
+                    time > 0
+                }
+
+                for (neighbor, time) in validNeighbors {
+                    graph.addEdge(from: vertex, to: neighbor, weight: time)
+                }
+            }
+
         }
     }
-    return graph
+
+    return (graph, gridDict)
 }
 
-func partTwo(input: Input){
-    let terrain = generateTerrain(x: input.x, y: input.y, depth: input.depth)
-    let graph = generateGraph(terrain: terrain)
+func partTwo(input: Input) {
+    let terrain = generateTerrain(x: input.x + 7, y: input.y + 7, depth: input.depth)
+    let (graph, gridDict) = generateGraph(terrain: terrain)
+    let target = gridDict[Point(x: input.x, y: input.y)]?.first { (vertex: Vertex) -> Bool in
+        vertex.tool == .Torch
+    }
 
-    print(graph)
+    let start = Vertex(x: 0, y: 0, type: .Rocky, tool: .Torch)
+
+    let (_, pathDict) = graph.dijkstra(root: start, startDistance: 0)
+    let startIndex = graph.indexOfVertex(start)!
+    let endIndex = graph.indexOfVertex(target!)!
+    let path = pathDictToPath(from: startIndex, to: endIndex, pathDict: pathDict)
+
+    let time = path.map { edge -> Int in
+        edge.weight
+    }.reduce(0, +)
+
+    print(time)
 }
+
+let testInput = Input(x: 10, y: 10, depth: 510)
+let realInput = Input(x: 7, y: 701, depth: 11394)
 
 //partOne(input: realInput)
 partTwo(input: testInput)
